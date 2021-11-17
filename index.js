4b1666de08e73246a57364ec46157b94278a29b9
emilyroseschmidt <92035095+emilyroseschmidt@users.noreply.github.com>
2021-11-17 12:27:24 +0000

    New code

---
+++ index.js
@@ -0,0 +1,22 @@
+const { request } = require("express");
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/quadratic", (req, res) => {
+
+function solve(a,b,c){
+    var result = (((-1*b) + Math.sqrt(Math.pow(b,2)) - (4*a*c))/(2*a));
+    var result2 = (((-1*b) - Math.sqrt(Math.pow(b,2)) - (4*a*c))/(2*a));
+    
+    return result + result2;
+}
+
+console.log(solve(1,1,-1));
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+  
